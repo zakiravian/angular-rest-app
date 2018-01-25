2fe29f74f3c8eecbffd5fe9de2208510fc8136e2
Zaki Ahmad <zakiravian@gmail.com>
2018-01-25 19:40:48 +0300

    Initial Commit
    Angular Training Initial Commit

--- src/app/shared/items.service.ts
+++ src/app/shared/items.service.ts
@@ -15,7 +15,7 @@ export class ItemsService {
     return this.http.get(BASE_URL)
       .map(res => res.json());
   }
-
+  
   load(id) {
     return this.http.get(`${BASE_URL}${id}`)
       .map(res => res.json());
